5a9d0466b3e2908f555575c22382c1ee08c21a9a
Madeeha-Anjum <maadeeeehaa@gmail.com>
2024-05-16 20:58:53 -0600

    sigleton pattern for settings

--- src/multi_tenancy_system/settings.py
+++ src/multi_tenancy_system/settings.py
@@ -1,57 +1,55 @@
-# TODO create a Singleton of the settings class then get tenant singleton
-
-
 import os
+from typing import ClassVar
 
-from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
-load_dotenv()  # take environment variables from .env.
-
-__all__ = ["get_settings"]
-
-
-class Settings(BaseSettings):
-    model_config = SettingsConfigDict(case_sensitive=True)
-    # Loaded from environment
-    ENVIRONMENT: str
-
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        extra="ignore",
-        env_file_encoding="utf-8",
-        case_sensitive=True,
-        populate_by_name=True,
-    )
-
-
-class Dev(BaseSettings):
-    # loaded from environment and .env file
-    BASE_DIR: str = os.path.dirname(os.path.abspath(__file__))
-    ENVIRONMENT: str
-    APP_NAME: str = "Multi tenancy API"
-    DB_USER: str
-    DB_PASS: str
-    DB_HOST: str
-    DB_PORT: str
-    DB_NAME: str
-
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        extra="ignore",
-        env_file_encoding="utf-8",
-        case_sensitive=True,
-        populate_by_name=True,
-    )
-
-
-settings = Settings()
 
 
 def get_settings():
-    environment = settings.ENVIRONMENT
+    environment = os.getenv("ENVIRONMENT")
+
     if not environment:
-        raise ValueError("ENVIRONMENT environment variable is not set")
+        raise ValueError("set ENVIRONMENT to 'development' or 'production'")
 
     if environment == "development":
-        return Dev()
+        return Settings.instance()
+    else:
+        raise ValueError("ENVIRONMENT variable is not set to development or production", environment)
+
+
+class Settings(BaseSettings):
+    SettingsConfigDict(
+        env_prefix="",  # prefix for environment variables
+        case_sensitive=True,
+        populate_by_name=True,
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+    ENVIRONMENT: str = "development"
+    BASE_DIR: str = os.path.dirname(os.path.abspath(__file__))
+    APP_NAME: str = "Multi tenancy API"
+
+    # loaded from environment and .env file
+    DB_USER: str
+    DB_NAME: str
+    DB_PASS: str
+    DB_HOST: str
+    DB_PORT: str
+
+    _instance: ClassVar[None] = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            print(f"\n{__name__}=====> \033[92mSettings __new__\033[0m\n")
+            cls._instance = super(Settings, cls).__new__(cls)
+            # Put any initialization here.
+            return cls._instance
+        return cls._instance
+
+    @classmethod
+    def instance(cls) -> "Settings":
+        print(f"\n{__name__}=====> \033[92mSettings instance\033[0m\n")
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
